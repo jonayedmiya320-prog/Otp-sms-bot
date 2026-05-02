a33724e627017d7fabcd76aa658a32f79a2414cc
jonayedmiya320-prog <jonayedmiya320@gmail.com>
2026-05-02 12:24:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import requests
 
 # Configuration
 MASDAR_URL = "http://139.99.69.196"
-USERNAME = "Waleedbhai"
-PASSWORD = "Waleedbhai"
+USERNAME = "Walei"
+PASSWORD = "Walei"
 
 # Telegram Configuration
 BOT_TOKEN = "8513071962:AAEuk7UOeKn1eV8rzCuB9B7giHbkAIudNGM"
