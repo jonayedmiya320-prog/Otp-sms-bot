9ed11e535c9b41acce78a9b02615958b04c5661b
jonayedmiya320-prog <jonayedmiya320@gmail.com>
2026-05-04 11:33:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ NUMBER_BOT_HTTP_URL = "https://t.me/Secure_otp_hub_bot"
 
 # Telegram Button URLs
 NUMBER_CHANNEL_URL = "https://t.me/EARNING_HUB_NUMBER_BOT"
-MAIN_CHANNEL_URL   = "https://t.me/https://t.me/spyxpremiumsmschats"
+MAIN_CHANNEL_URL   = "https://t.me/spyxpremiumsmschats"
 
 # Headers
 HEADERS = {
