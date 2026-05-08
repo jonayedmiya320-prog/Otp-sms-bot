d6bfae7c329f1ec3de6151a45db087752cdd40e5
jonayedmiya320-prog <jonayedmiya320@gmail.com>
2026-05-08 17:12:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from telegram.ext import (
 # Configuration
 MASDAR_URL = "http://139.99.69.196"
 USERNAME = "Waleedbhai"
-PASSWORD = "Waleedbhai"
+PASSWORD = "Waleedbha"
 
 # Telegram Configuration
 BOT_TOKEN = "8513071962:AAEuk7UOeKn1eV8rzCuB9B7giHbkAIudNGM"
